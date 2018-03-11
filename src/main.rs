5f0d0b35065e4e34a1f302a93227b7ba735dcdc5
Don McCaughey <don@donm.cc>
2018-03-11 10:58:29 -0700

    Create struct Story and serialize to JSON.
    Created the Story struct to capture the important parts of each RSS feed
    item.
    
    Built a vector of Story structs created from RSS Item structs and
    serialize it to JSON.
    
    Changed type of `link' and `comments' fields on Item and Story to Url.
    Changed type of `pub_date' and `date' fields to DateTime<Utc> on Item
    and Story respectively.

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,97 @@
 #[macro_use]
 extern crate serde_derive;
 
+extern crate chrono;
 extern crate futures;
 extern crate hyper;
 extern crate hyper_tls;
 extern crate native_tls;
 extern crate serde;
+extern crate serde_json;
 extern crate serde_xml_rs;
 extern crate tokio_core;
+extern crate url;
+extern crate url_serde;
 
 
-use std::io::{self, Write};
+use chrono::{DateTime, Utc};
 use futures::Stream;
 use hyper::Client;
 use hyper_tls::HttpsConnector;
+use std::io::{self, Write};
 use tokio_core::reactor::Core;
 
 
-#[derive(Deserialize, Debug)]
+mod rfc_822_format {
+    use chrono::{DateTime, Utc, TimeZone};
+    use serde::{self, Deserialize, Serializer, Deserializer};
+
+    // Sun, 11 Mar 2018 00:55:15 +0000
+    // day, dd mmm yyyy hh:mm:ss +zzzz
+    const FORMAT: &'static str = "%a, %e %b %Y %H:%M:%S %z";
+
+    pub fn serialize<S>(date: &DateTime<Utc>, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer
+    {
+        let s = format!("{}", date.format(FORMAT));
+        serializer.serialize_str(&s)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<DateTime<Utc>, D::Error>
+        where D: Deserializer<'de>
+    {
+        let s = String::deserialize(deserializer)?;
+        Utc.datetime_from_str(&s, FORMAT).map_err(serde::de::Error::custom)
+    }
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
 struct RSSFeed {
     channel: Channel,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Channel {
-    title: String,
-    link: String,
     description: String,
     #[serde(rename = "item")]
     items: Vec<Item>,
+    #[serde(with = "url_serde")]
+    link: url::Url,
+    title: String,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Item {
-    title: String,
-    link: String,
-    #[serde(rename = "pubDate")]
-    pub_date: String,
-    comments: String,
+    #[serde(with = "url_serde")]
+    comments: url::Url,
     description: String,
+    #[serde(with = "url_serde")]
+    link: url::Url,
+    #[serde(rename = "pubDate", with = "rfc_822_format")]
+    pub_date: DateTime<Utc>,
+    title: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Story {
+    #[serde(with = "url_serde")]
+    comments: url::Url,
+    date: DateTime<Utc>,
+    #[serde(with = "url_serde")]
+    link: url::Url,
+    title: String,
+}
+
+impl Story {
+    fn from_item(item: &Item) -> Story {
+        Story {
+            comments: item.comments.clone(),
+            date: item.pub_date.clone(),
+            link: item.link.clone(),
+            title: item.title.clone(),
+        }
+    }
 }
 
 
@@ -91,4 +144,19 @@ fn main() {
         println!("        description: {}", item.description);
         println!("");
     }
+
+    let mut stories: Vec<Story> = Vec::new();
+    for item in rss_feed.channel.items.iter() {
+        let story = Story::from_item(&item);
+        stories.push(story);
+    }
+
+    let stories_json = match serde_json::to_string_pretty(&stories) {
+        Ok(stories_json) => stories_json,
+        Err(error) => {
+            println!("ERROR: {:?}", error);
+            return;
+        }
+    };
+    println!("{}", stories_json);
 }
