655cd74bbcd19b2ff94ec820c4ffc5e78dd9d712
Don McCaughey <don@donm.cc>
2018-04-01 22:02:34 -0700

    Move struct RSS into submodule `rss'.
    And re-export it as `rss::RSS'.

---
+++ src/bin/news_extractor/rss/rss.rs
@@ -0,0 +1,121 @@
+use chrono::DateTime;
+use chrono::Utc;
+use error::Error;
+use news::Story;
+use serde_json;
+use std::fs::create_dir_all;
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::path::Path;
+use super::Channel;
+
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct RSS {
+    pub channel: Channel,
+}
+
+impl RSS {
+    pub fn create_stories(&self, created_date: DateTime<Utc>) -> Vec<Story> {
+        self.channel.items.iter()
+            .map(|item| item.to_story(created_date))
+            .collect()
+    }
+
+    pub fn write(&self, path: &Path) -> Result<(), Error> {
+        match path.parent() {
+            Some(parent) => create_dir_all(parent).map_err(Error::Io)?,
+            None => return Err(Error::invalid_path(path)),
+        };
+        let json = serde_json::to_string_pretty(self)
+            .map_err(Error::JsonConversion)?;
+        let mut file = OpenOptions::new()
+            .create(true).truncate(true).write(true)
+            .open(path).map_err(Error::Io)?;
+        file.write_all(json.as_bytes())
+            .map_err(Error::Io)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use serde_xml_rs;
+    use super::*;
+
+    #[test]
+    fn test_rss_from_xml() {
+        let xml = r#"
+            <rss version="2.0">
+                <channel>
+                    <title>News Channel</title>
+                    <link>https://news.example.com/news_channel</link>
+                    <description>Better than teevee.</description>
+                    <item>\
+                        <title>Article One</title>\
+                        <link>https://news.example.com/article1</link>\
+                        <pubDate>Sun, 18 Mar 2018 23:43:03 +0000</pubDate>\
+                        <comments>https://news.example.com/article1/comments</comments>\
+                        <description><![CDATA[<a href="https://news.example.com/article1/comments">Comments</a>]]></description>\
+                    </item>
+                </channel>
+            </rss>
+        "#;
+        let rss: RSS = serde_xml_rs::deserialize(xml.as_bytes()).unwrap();
+        assert_eq!("News Channel", rss.channel.title);
+        assert_eq!("https://news.example.com/news_channel", rss.channel.link.as_str());
+        assert_eq!("Better than teevee.", rss.channel.description);
+        assert_eq!(1, rss.channel.items.len());
+
+        assert_eq!("Article One", rss.channel.items[0].title);
+        assert_eq!("https://news.example.com/article1", rss.channel.items[0].link.as_str());
+        assert_eq!(1521416583, rss.channel.items[0].pub_date.timestamp());
+        assert_eq!("https://news.example.com/article1/comments", rss.channel.items[0].comments.as_str());
+        assert_eq!(r#"<a href="https://news.example.com/article1/comments">Comments</a>"#, rss.channel.items[0].description);
+    }
+
+    #[test]
+    fn test_rss_create_stories() {
+        let xml = r#"
+            <rss version="2.0">
+                <channel>
+                    <title>News Channel</title>
+                    <link>https://news.example.com/news_channel</link>
+                    <description>Better than teevee.</description>
+                    <item>\
+                        <title>Article One</title>\
+                        <link>https://news.example.com/article1</link>\
+                        <pubDate>Sun, 18 Mar 2018 23:43:03 +0000</pubDate>\
+                        <comments>https://news.example.com/article1/comments</comments>\
+                        <description><![CDATA[<a href="https://news.example.com/article1/comments">Comments</a>]]></description>\
+                    </item>
+                    <item>\
+                        <title>Article Two</title>\
+                        <link>https://news.example.com/article2</link>\
+                        <pubDate>Sun, 18 Mar 2018 13:59:55 +0000</pubDate>\
+                        <comments>https://news.example.com/article2/comments</comments>\
+                        <description><![CDATA[<a href="https://news.example.com/article2/comments">Comments</a>]]></description>\
+                    </item>
+                </channel>
+            </rss>
+        "#;
+        let rss: RSS = serde_xml_rs::deserialize(xml.as_bytes()).unwrap();
+        let created_date = DateTime::parse_from_rfc2822("Thu, 22 Mar 2018 13:08:18 +0000")
+            .unwrap().with_timezone(&Utc);
+        let stories = rss.create_stories(created_date);
+
+        assert_eq!(2, stories.len());
+
+        assert_eq!("Article One", stories[0].title);
+        assert_eq!("https://news.example.com/article1", stories[0].link.as_str());
+        assert_eq!(created_date, stories[0].created_date);
+        assert_eq!(1521416583, stories[0].pub_date.timestamp());
+        assert_eq!("https://news.example.com/article1/comments", stories[0].comments.as_str());
+
+        assert_eq!("Article Two", stories[1].title);
+        assert_eq!("https://news.example.com/article2", stories[1].link.as_str());
+        assert_eq!(created_date, stories[1].created_date);
+        assert_eq!(1521381595, stories[1].pub_date.timestamp());
+        assert_eq!("https://news.example.com/article2/comments", stories[1].comments.as_str());
+    }
+}
