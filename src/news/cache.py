c117dd598b3ee75b4ea9312d8dfc5e9540d8fd38
Don McCaughey <don@donm.cc>
2022-12-20 23:59:05 -0800

    In news, cache news file contents in memory.
    Modify the `Cache` class to check the file modification time before
    re-reading the news file from disk.

--- src/news/cache.py
+++ src/news/cache.py
@@ -4,14 +4,20 @@ from pathlib import Path
 class Cache:
     def __init__(self, path: Path):
         self.path = path
+        self.mtime = None
+        self.contents = ''
 
     def get(self) -> str:
         if self.path.is_file():
-            with self.path.open('r', encoding='utf-8') as f:
-                return f.read()
-        else:
-            return ''
+            mtime = self.path.stat().st_mtime
+            if mtime != self.mtime:
+                with self.path.open('r', encoding='utf-8') as f:
+                    self.contents = f.read()
+                self.mtime = mtime
+        return self.contents
 
     def put(self, json: str):
         with self.path.open('w', encoding='utf-8') as f:
             f.write(json)
+        self.mtime = self.path.stat().st_mtime
+        self.contents = json
