a1aed9f7567c9572e217f02965e82b9daf197002
Don McCaughey <don@donm.cc>
2022-12-24 19:51:08 -0800

    Add news query script.
    Implement `query.py` for news to dump a sorted list of news URLs.
    
    Add the `__lt__()` method to `URL` to make a list of `URL` objects
    sortable.

--- src/news/url.py
+++ src/news/url.py
@@ -8,6 +8,9 @@ class URL:
     def __hash__(self) -> int:
         return hash(self.url)
 
+    def __lt__(self, other: 'URL') -> bool:
+        return self.url < other.url
+
     def __repr__(self) -> str:
         return f"URL('{self.url}')"
 
---
+++ src/query.py
@@ -0,0 +1,35 @@
+import argparse
+
+from news import Cache, News, NoStore, S3Store
+from pathlib import Path
+
+
+def parse_options():
+    arg_parser = argparse.ArgumentParser(description='Query news.')
+    arg_parser.add_argument('-c', '--cache-path', dest='cache_path',
+                            default='./news.json', type=Path,
+                            help='location to store news items')
+    arg_parser.add_argument('--no-store', dest='no_store', default=False,
+                            action='store_true', help="don't use a persistent store")
+    options = arg_parser.parse_args()
+    return options
+
+
+def main():
+    options = parse_options()
+
+    if options.no_store:
+        json = Cache(options.cache_path).get()
+    else:
+        json = S3Store().get()
+
+    news = News.from_json(json)
+
+    urls = [item.url for item in news]
+    urls.sort()
+    for url in urls:
+        print(url)
+
+
+if __name__ == '__main__':
+    main()
