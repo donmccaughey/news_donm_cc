d01417699a9b7ab00f19de56ecf3e09538b0cfc4
Don McCaughey <don@donm.cc>
2023-05-06 15:50:38 -0700

    In news, add more sites to the skip list.

--- src/feeds/skip_sites.py
+++ src/feeds/skip_sites.py
@@ -1,21 +1,37 @@
 SKIP_SITES = [
     'brave.com',
+
+    'emergencemagazine.org',
     'english.elpais.com',
     'europeanreviewofbooks.com',
+
+    'jacobin.com',
     'jewishreviewofbooks.com',
+
     'laphamsquarterly.org',
     'lareviewofbooks.org',
+    'lithub.com',
+    'longreads.com',
     'lynalden.com',
+
     'narratively.com',
     'newscientist.com',
     'newstatesman.com',
     'noahpinion.blog',
+    'nplusonemag.com',
+
     'paulgraham.com',
+
     'quillette.com',
+
     'reason.com',
+
+    'seths.blog',
     'sive.rs',
     'astralcodexten.substack.com',
     'noahpinion.substack.com',
+
     'theamericanscholar.org',
     'theepochtimes.com',
+    'theparisreview.org',
 ]
