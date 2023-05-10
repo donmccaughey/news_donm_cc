5c6aa28e50e1bb6d6261668cd45980d19cd5bb40
Don McCaughey <don@donm.cc>
2023-05-09 21:21:38 -0700

    Parameterise df tests in news.
    Consolidate the various tests for the `keep_entry()` method of the
    `DaringFireball` class into parameterized tests, one set for accepted
    entries and one for rejected entries.

--- src/feeds/daring_fireball_test.py
+++ src/feeds/daring_fireball_test.py
@@ -1,42 +1,8 @@
+import pytest
 from feedparser import FeedParserDict, parse
+from pytest import mark
 
 from .daring_fireball import DaringFireball
-
-
-def test_keep_entry_keeps_essay():
-    d = build_df_feed(
-        alternate='https://daringfireball.net/2022/11/twitter_tumult',
-        related='',
-        title='★ Twitter Tumult',
-    )
-    entry = d.entries[0]
-    df = DaringFireball({})
-
-    assert df.keep_entry(entry)
-
-
-def test_keep_entry_keeps_linked():
-    d = build_df_feed(
-        alternate='https://www.forbes.com/sites/emilybaker-white/2022/12/22/tiktok-tracks-forbes-journalists-bytedance/',
-        related='https://daringfireball.net/linked/2022/12/22/tiktok-forbes-spying',
-        title='TikTok Spied on Forbes Journalists',
-    )
-    entry = d.entries[0]
-    df = DaringFireball({})
-
-    assert df.keep_entry(entry)
-
-
-def test_keep_entry_rejects_linked_sponsor():
-    d = build_df_feed(
-        alternate='https://workos.com/?utm_source=daringfireball&amp;utm_medium=newsletter&amp;utm_campaign=df2023',
-        related='https://daringfireball.net/linked/2023/02/12/workos',
-        title='WorkOS',
-    )
-    entry = d.entries[0]
-    df = DaringFireball({})
-
-    assert not df.keep_entry(entry)
 
 
 def test_is_entry_valid_rejects_no_title():
@@ -59,48 +25,72 @@ def test_is_entry_valid_rejects_no_links():
     assert not df.is_entry_valid(entry)
 
 
-def test_keep_entry_rejects_sponsors_alternate_link():
+ACCEPT_SITE_TESTS = [
+    pytest.param(
+        'https://daringfireball.net/2022/11/twitter_tumult',
+        '',
+        '★ Twitter Tumult',
+        id='essay',
+    ),
+    pytest.param(
+        'https://www.forbes.com/sites/emilybaker-white/2022/12/22/tiktok-tracks-forbes-journalists-bytedance/',
+        'https://daringfireball.net/linked/2022/12/22/tiktok-forbes-spying',
+        'TikTok Spied on Forbes Journalists',
+        id='link',
+    ),
+]
+
+
+@mark.parametrize('alternate, related, title', ACCEPT_SITE_TESTS)
+def test_keep_entry_accepts_link(alternate, related, title):
     d = build_df_feed(
-        alternate='https://daringfireball.net/feeds/sponsors/',
-        related='https://daringfireball.net/linked/2023/04/22/df-sponsorship-openings',
-        title='DF Sponsorship Openings',
+        alternate='https://daringfireball.net/2022/11/twitter_tumult',
+        related='',
+        title='★ Twitter Tumult',
     )
     entry = d.entries[0]
     df = DaringFireball({})
 
-    assert not df.keep_entry(entry)
+    assert df.keep_entry(entry)
 
 
-def test_keep_entry_rejects_sponsors_related_link():
-    d = build_df_feed(
-        alternate='https://retool.com/?utm_source=sponsor&amp;utm_medium=newsletter&amp;utm_campaign=daringfireball',
-        related='https://daringfireball.net/feeds/sponsors/2022/12/retool_5',
-        title='[Sponsor] Retool',
-    )
-    entry = d.entries[0]
-    df = DaringFireball({})
+REJECT_SITE_TESTS = [
+    pytest.param(
+        'https://workos.com/?utm_source=daringfireball&amp;utm_medium=newsletter&amp;utm_campaign=df2023',
+        'https://daringfireball.net/linked/2023/02/12/workos',
+        'WorkOS',
+        id='linked sponsor',
+    ),
+    pytest.param(
+        'https://daringfireball.net/feeds/sponsors/',
+        'https://daringfireball.net/linked/2023/04/22/df-sponsorship-openings',
+        'DF Sponsorship Openings',
+        id='sponsors alternate link',
+    ),
+    pytest.param(
+        'https://retool.com/?utm_source=sponsor&amp;utm_medium=newsletter&amp;utm_campaign=daringfireball',
+        'https://daringfireball.net/feeds/sponsors/2022/12/retool_5',
+        '[Sponsor] Retool',
+        id='sponsors related link',
+    ),
+    pytest.param(
+        'https://daringfireball.net/thetalkshow/2022/12/22/ep-365',
+        'https://daringfireball.net/linked/2022/12/22/the-talk-show-365',
+        'The Talk Show: ‘Permanent September’',
+        id='The Talk Show link',
+    ),
+    pytest.param(
+        'https://dithering.fm/',
+        'https://daringfireball.net/linked/2023/02/03/dithering',
+        'Dithering',
+        id='Dithering link',
+    ),
+]
 
-    assert not df.keep_entry(entry)
 
-
-def test_keep_entry_rejects_the_talk_show_link():
-    d = build_df_feed(
-        alternate='https://daringfireball.net/thetalkshow/2022/12/22/ep-365',
-        related='https://daringfireball.net/linked/2022/12/22/the-talk-show-365',
-        title='The Talk Show: ‘Permanent September’',
-    )
-    entry = d.entries[0]
-    df = DaringFireball({})
-
-    assert not df.keep_entry(entry)
-
-
-def test_keep_entry_rejects_dithering_link():
-    d = build_df_feed(
-        alternate='https://dithering.fm/',
-        related='https://daringfireball.net/linked/2023/02/03/dithering',
-        title='Dithering',
-    )
+@mark.parametrize('alternate, related, title', REJECT_SITE_TESTS)
+def test_keep_entry_rejects_link(alternate, related, title):
+    d = build_df_feed(str(alternate), str(related), str(title))
     entry = d.entries[0]
     df = DaringFireball({})
 
