c1071a417b9587190cd55500681820cd49f64767
Don McCaughey <don@donm.cc>
2023-06-19 17:28:33 -0700

    Add a test to verify that `URL` objects sort as expected.
    I already implemented `__lt__()` for `URL`, so this verifies it.

--- src/news/url_test.py
+++ src/news/url_test.py
@@ -13,6 +13,20 @@ def test_eq_and_hash():
     assert url1 != url2
 
 
+def test_lt_by_sorting():
+    url1 = URL('https://aaa.com/1')
+    url2 = URL('https://bbb.com/1')
+    url3 = URL('https://ccc.com/2')
+    url4 = URL('https://ddd.com/2')
+
+    sorted_urls = sorted([url3, url1, url4, url2])
+
+    assert sorted_urls[0] == url1
+    assert sorted_urls[1] == url2
+    assert sorted_urls[2] == url3
+    assert sorted_urls[3] == url4
+
+
 def test_str_and_repr():
     url = URL('https://www.example.com/foo/bar?baz#fid')
 
