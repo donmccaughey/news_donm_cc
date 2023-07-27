b4f79d0f1c290453b2c03c856c6fc70074fb6aec
Don McCaughey <don@donm.cc>
2023-07-27 14:38:41 -0700

    Update the query script.
    Add filtering by site ID ("df", "hn", "lob", etc) and add more fields to
    the output of the "urls-by-source-counts" query.

--- src/query.py
+++ src/query.py
@@ -2,7 +2,7 @@ import argparse
 from collections import defaultdict
 from pathlib import Path
 
-from news import News
+from news import Item, News
 from extractor import S3Store
 from utility import Cache
 
@@ -11,6 +11,9 @@ def parse_options():
     arg_parser = argparse.ArgumentParser(description='Query news.')
     arg_parser.add_argument('-o', '--open', dest='news_path', type=Path,
                             help='location of news items JSON')
+    arg_parser.add_argument('--site-id', dest='site_ids',
+                            type=str, action='append',
+                            help='site ID to include in results ("df", "hn", "lob", ...)')
 
     query_choices = ['urls', 'urls-by-site', 'urls-by-source-counts']
     arg_parser.add_argument('query', metavar='QUERY',
@@ -41,10 +44,14 @@ def query_urls_by_site(news):
 
 
 def query_urls_by_source_counts(news):
-    by_count = [(item.count, item.url) for item in news]
-    by_count.sort(key=lambda t: (-t[0], str(t[1])))
-    for count, url in by_count:
-        print(f'{count}: {url}')
+    by_count = list(news)
+    by_count.sort(key=lambda item: (-item.count, item.sources[0].site_id, item.title))
+    w1 = w2 = 0
+    for item in by_count:
+        w1 = max(w1, len(item.sources[0].site_id))
+        w2 = max(w2, len(item.url.identity))
+    for item in by_count:
+        print(f'{item.count:4}\t{item.sources[0].site_id:{w1}}\t{item.url.identity:{w2}}\t{item.title}')
 
 
 def main():
@@ -56,6 +63,15 @@ def main():
         json = S3Store().get()
 
     news = News.from_json(json)
+
+    if options.site_ids:
+        def from_site(item: Item):
+            for source in item.sources:
+                if source.site_id in options.site_ids:
+                    return True
+            return False
+
+        news = filter(from_site, news)
 
     match options.query:
         case 'urls':
